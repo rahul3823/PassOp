388f1682d2bbf777dc8ad62598091752658cec74
Rahul R Choudhary <rcc3823@gmail.com>
2025-07-06 23:39:07 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
                     </li>
                 </ul> */}
                 <button className='text-white bg-green-700 my-5 mx-2 rounded-full flex  justify-between items-center ring-white ring-1'> 
-                    <img className='invert  w-10 p-1' src="/icons/github.svg" alt="github logo" />
+                    <img className='invert  w-10 p-1' src="/icons/icons8-github-48.png" alt="github logo" />
                     <span className='font-bold px-2'>GitHub</span>
                     
                 </button>
